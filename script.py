808253e4428c83d0355050f4f2cdd2799d94b925
tsai.yw <tsai.yw@tsaiyw-7215-01.srrcnt.org.tw>
2024-08-21 10:51:50 +0800

    add example with zoneplate

--- script.py
+++ script.py
@@ -8,17 +8,27 @@ raw_data = eiger.load_bluesky_exp_data(scan_file_path)
 
 ## set data pretreatment config
 data_pretreatment_config = pp.data_pretreatment_config()
-data_pretreatment_config.clip_xcen = 'auto' # 'auto' for adapting xcen from h5 header or a integer for setting manually 
-data_pretreatment_config.clip_ycen = 'auto' # 'auto' for adapting ycen from h5 header or a integer for setting manually 
-data_pretreatment_config.clip_size = 401 # must be a odd number
-data_pretreatment_config.saturation_threshold = 'auto' # 'auto' for adapting SaturationIntensity from h5 header or a integer for setting manually 
-data_pretreatment_config.binning = 2 # binning factor
+data_pretreatment_config.clip_xcen              = 'auto' # 'auto' for adapting xcen from h5 header or a integer for setting manually 
+data_pretreatment_config.clip_ycen              = 'auto' # 'auto' for adapting ycen from h5 header or a integer for setting manually 
+data_pretreatment_config.clip_size              = 401 # must be a odd number
+data_pretreatment_config.saturation_threshold   = 'auto' # 'auto' for adapting SaturationIntensity from h5 header or a integer for setting manually 
+data_pretreatment_config.binning                = 2 # binning factor
 ## pretreat data
 pretreated_data = pp.pretreat_data(raw_data,data_pretreatment_config)
 
 ## create probe
 probe_gen_config = pp.probe_gen_config()
-probe_gen_config.mixture_state = 3
+probe_gen_config.mixture_state                  = 3
+probe_gen_config.gen_method                     = 'zoneplate' #'zoneplate'
+
+# for zoneplate config
+probe_gen_config.zoneplate_config.dr            = 70e-9 # in meter
+probe_gen_config.zoneplate_config.N             = 286
+probe_gen_config.zoneplate_config.energy        = pretreated_data.energy
+probe_gen_config.zoneplate_config.thickness     = 1500e-6 # in meter
+probe_gen_config.zoneplate_config.material      = 'Au'
+probe_gen_config.zoneplate_config.defocal       = 500e-6 # in meter
+
 probe = pp.gen_probe(pretreated_data,probe_gen_config)
 
 ## create object
